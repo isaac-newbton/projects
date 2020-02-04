5c05e3f12675004afecc7beb17edb12adb050a8a
BrandynL <brandyn.lordi@gmail.com>
2020-02-04 16:08:37 -0500

    added create project enpoint

--- src/Controller/Api/ProjectApiController.php
+++ src/Controller/Api/ProjectApiController.php
@@ -1,15 +1,41 @@
 <?php
 namespace App\Controller\Api;
 
+use App\Entity\Project;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
-class ProjectApiController {
+class ProjectApiController extends AbstractController {
 	/**
 	 * @Route("/api/v1/project/create")
 	 */
-	public function createProject(){
-		return new JsonResponse("TODO: create project");
+	public function createProject(Request $request){
+		$name = $request->get("name");
+		$dueDate = $request->get("dueDate");
+
+		if (!$name) return new JsonResponse(["error" => "project name is required"], 418); // LOL
+
+		$em = $this->getDoctrine()->getManager();
+		
+		$project = new Project();
+		$project->setName($name);
+		$project->setDueDate($dueDate);
+
+
+		$em->persist($project);
+		$em->flush();
+		$em->refresh($project);
+
+		return new JsonResponse([
+			"id" => $project->getId(),
+			"name" => $project->getName(),
+			"uuid" => $project->getUuid(),
+			"viewUuid" => $project->getViewUuid(),
+			"editUuid" => $project->getEditUuid(),
+		], 200);
+
 	}
 	/**
 	 * @Route("/api/v1/project/view")
