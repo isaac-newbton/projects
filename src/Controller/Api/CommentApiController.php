fc7670dce66fd9ff7c8109019dee808414c85910
BrandynL <brandyn.lordi@gmail.com>
2020-02-18 16:08:44 -0500

    added comment create endpoint

--- src/Controller/Api/CommentApiController.php
+++ src/Controller/Api/CommentApiController.php
@@ -1,16 +1,37 @@
 <?php
 namespace App\Controller\Api;
 
+use App\Entity\Comment;
+use App\Entity\Task;
+use App\Repository\TaskRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 
 class CommentApiController extends AbstractController {
 
 	/**
 	 * @Route("/api/v1/comment/create", methods={"POST"})
 	 */
-	public function createComment(){
-		return new JsonResponse("TODO: create comment and add to a specifed task");
+	public function createComment(Request $request, TaskRepository $taskRepository){
+		$data = json_decode($request);
+		if (!$this->getUser()) return new JsonResponse(['error' => 'user must be authenticated']);
+		if (!$data->content) return new JsonResponse(['error' => 'content is a required']);
+		if (!$data->taskUuid) return new JsonResponse(['error' => 'task uuid is required']);
+
+		if (!$task = $taskRepository->findOneByEncodedUuid($data->taskUuid)) return new JsonResponse(['error' => 'task not found']);
+
+		$em = $this->getDoctrine()->getManager();
+		$comment = new Comment();
+		$comment->setTask($task);
+		$comment->setUser($this->getUser());
+		$comment->setContent($data->content);
+
+		$em->persist($comment);
+		$em->merge($task);
+		$em->flush();
+
+		return new JsonResponse(['success'], 200);
 	}
 
 	/**
