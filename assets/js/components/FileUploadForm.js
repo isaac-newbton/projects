93cbdc9be92c16fb959afa2ee4638cb11de4bd6a
BrandynL <brandyn.lordi@gmail.com>
2020-02-20 14:29:20 -0500

    add file upload form and display queued files

--- assets/js/components/FileUploadForm.js
+++ assets/js/components/FileUploadForm.js
@@ -1,29 +1,37 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { Form, Button } from 'react-bootstrap';
-import { Label } from 'react-bootstrap';
+import { Form, Button, Badge } from 'react-bootstrap';
 
 const FileUploadForm = ({handleSubmit}) => {
 	const [files, setFiles] = useState([])
 
-	const handleFiles = file => {
-		console.log(file)
-	}
+	const handleAddFiles = fileList => setFiles([...files, ...(Object.values(fileList).filter( file => !files.find(files => files.name === file.name) )) ])
 
 	const handleFormSubmit = e => {
 		e.preventDefault()
-		setFiles(null)
-		handleSubmit(files)
+		console.log(files)
 	}
 
+	const UploadButton = () => files.length > 0 ? <Button type="submit">Upload Selected Files</Button> : null;
+	const QueuedFiles = () => files.map((file, index) => {
+		return (
+			<div key={index}>
+			<Button size="sm" className='text-danger' variant="link">&times;</Button>
+			{file.name}
+			</div>
+		)
+	})
+
 	if (handleSubmit){
+		// console.log(files)
 		return (
 			<Form onSubmit={e => handleFormSubmit(e)}>
 				<Form.Group controlId="fileUpload">
-					<Form.Label controlId="fileUpload">Select File(s)</Form.Label>
-					<Form.Control style={{display:'none'}} placeholder="Upload" onChange={e => handleFiles(e.target.files)} type="file"></Form.Control>
+					<Form.Label controlId="fileUpload" className="btn btn-link">Add File(s)</Form.Label>
+					<Form.Control style={{display:'none'}} multiple={true} placeholder="Upload" onChange={e => handleAddFiles(e.target.files)} type="file"></Form.Control>
+					<QueuedFiles />
+					<UploadButton />
 				</Form.Group>
-				<Button type="submit" className="">Upload</Button>
 			</Form>
 		)
 	}
