75af537e2dcd80c5051113e52f9955281014b70c
BrandynL <brandyn.lordi@gmail.com>
2020-03-04 14:00:43 -0500

    added header

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,71 +8,76 @@ import NewUser from './screens/NewUser';
 import SignIn from './screens/LogIn';
 import { useEffect } from 'react';
 import LogIn from './screens/LogIn';
+import Header from './components/Header';
 
 const App = () => {
-
-	const [user, setUser] = useState(null)
+	const [user, setUser] = useState(null);
 
 	useEffect(() => {
 		// TODO: put this in an auth service component
-		fetch("/api/v1/auth")
-		.then(resp => resp.json())
-		.then(resp => !resp.error ? setUser(resp) : null)
-	} , [])
-	
+		fetch('/api/v1/auth')
+			.then(resp => resp.json())
+			.then(resp => (!resp.error ? setUser(resp) : null));
+	}, []);
+
 	const handleLogin = data => {
 		// TODO: put this in an auth service component ?
-		fetch("/api/v1/login", {
-			method: "POST",
+		fetch('/api/v1/login', {
+			method: 'POST',
 			headers: {
-				'Accept': 'application/json',
-      			'Content-Type': 'application/json'
+				Accept: 'application/json',
+				'Content-Type': 'application/json',
 			},
-			body: JSON.stringify(data)
+			body: JSON.stringify(data),
 		})
-		.then(resp => resp.json())
-		.then(resp => !resp.error ? setUser(resp) : console.log(resp))
-	}
+			.then(resp => resp.json())
+			.then(resp => (!resp.error ? setUser(resp) : console.log(resp)));
+	};
 
 	const LogOut = props => {
-		const [loggedOut, setLoggedOut] = useState(null)
-		fetch("/api/v1/logout")
-		.then(resp => resp.json())
-		.then(resp => {
-			if(!resp.error){
-				setLoggedOut(true)
-			}
-		})
-		return loggedOut ? <Redirect to="/login" /> : <p>Logging out...</p>
-	}
+		const [loggedOut, setLoggedOut] = useState(null);
+		fetch('/api/v1/logout')
+			.then(resp => resp.json())
+			.then(resp => {
+				if (!resp.error) {
+					setLoggedOut(true);
+				}
+			});
+		return loggedOut ? <Redirect to='/login' /> : <p>Logging out...</p>;
+	};
 
 	return (
-		<BrowserRouter>
-		<Switch>
-			<Route exact path="/">
-				<NewProject/>
-			</Route>
-			<Route path="/signup">
-				<NewUser/>
-			</Route>
-			<Route path="/login">
-				{user ? <Redirect to="/" /> : <LogIn handleLogin={handleLogin} />}
-			</Route>
-			<Route path="/logout">
-				<LogOut />
-			</Route>
-			<Route path="/project/:encodedUuid">
-				<ProjectScreen />
-			</Route>
-			<Route path="/task/:encodedUuid">
-				<TaskScreen/>
-			</Route>
-			<Route>
-				TODO: create a 404 component here
-			</Route>
-		</Switch>
-		</BrowserRouter>
-	)
-}
+		<>
+			<Header user={user} />
+			<BrowserRouter>
+				<Switch>
+					<Route exact path='/'>
+						<NewProject />
+					</Route>
+					<Route path='/signup'>
+						<NewUser />
+					</Route>
+					<Route path='/login'>
+						{user ? (
+							<Redirect to='/' />
+						) : (
+							<LogIn handleLogin={handleLogin} />
+						)}
+					</Route>
+					<Route path='/logout'>
+						<LogOut />
+					</Route>
+					<Route path='/project/:encodedUuid'>
+						<ProjectScreen />
+					</Route>
+					<Route path='/task/:encodedUuid'>
+						<TaskScreen />
+					</Route>
+					<Route>TODO: create a 404 component here</Route>
+				</Switch>
+			</BrowserRouter>
+		</>
+	);
+};
 
-ReactDOM.render(<App/>, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('root'));
